3fbfd873b528bfec83ff587dbbc226e17dd313ce
Laytdesu <134925653+Laytdesu@users.noreply.github.com>
2025-10-26 16:47:53 +0200

    Added most of the functions except isValidEmail and buySkyrim

--- Lesson6/Lesson6.cpp
+++ Lesson6/Lesson6.cpp
@@ -1,109 +1,143 @@
-﻿/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
-// Створити структуру, в котрій
-    // Змінні: нікнейм, пошта, пароль, баланс, bool isLogIn
-    // Функції: signIn, showInfo, bool isValidEmail/Password, LogIn,buySkirym, 
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
 
-struct Account {
+struct Account
+{
 private:
-    string username;
-    string email;
-    string password;
-    int balance = 0;
-    bool isLogIn = false;
+	string username;
+	string email;
+	string password;
+	string emailLogIn;
+	string passwordLogIn;
+	int balance = 0;
+	bool isLogIn = false;
 public:
-    void signUp()
-    {
-        cout << "Please enter your credentials to create and account" << endl;
-        cout << "Enter your E-Mail: ";
-        cin >> email;
-    }
+	void signUp()
+	{
+		if (!isLogIn) {
+			cout << "Please enter your E-Mail to create an account" << endl;
+			cout << "Enter your E-Mail: ";
+			cin >> email;
+			cout << "Please enter the desired password" << endl;
+			cout << "Enter the password: ";
+			cin >> password;
+			cout << "Please enter the desired username" << endl;
+			cout << "Enter the username: ";
+			cin >> username;
+			isLogIn = true;
+		}
+		else {
+			cout << "Oops, an account for this user already exists. Use the Log In option instead" << endl;
+		}
+	}
 
-    void showInfo()
-    {
-        cout << "User Information";
-    }
+	void showInfo()
+	{
+		if (isLogIn) {
+			cout << "User Information" << endl;
+			cout << "Username: " << username << endl;
+			cout << "E-Mail: " << email << endl;
+			cout << "Current balance: " << balance << endl;
+		}
+		else {
+			cout << "Error, could not display information because user is not logged in" << endl;
+		}
+	}
 
-    bool isValidEmail(string emailCheck)
-    {
-        return false;
-    }
+	bool isValidEmail(string emailCheck)
+	{
+		return false;
+	}
 
-    bool isValidPassword(string passwordCheck)
-    {
+	bool isValidPassword(string passwordCheck)
+	{
 
-        // valid: 
-         // 1. 8-64 size
-         // 2. Мінімум: 2 великих літери, 2 малі літери, 2 цифри, символ
-         // 3. не повинно бути пробілів
-        int lettersUpper = 0;
-        int lettersLower = 0;
-        int symbols = 0;
-        int numbers = 0;
+		// valid: 
+		 // 1. 8-64 size
+		 // 2. Мінімум: 2 великих літери, 2 малі літери, 2 цифри, символ
+		 // 3. не повинно бути пробілів
+		int lettersUpper = 0;
+		int lettersLower = 0;
+		int symbols = 0;
+		int numbers = 0;
 
-        if (passwordCheck.length() < 8 || passwordCheck.length() > 64) {
-            cout << "Length of password is invalid" << endl;
-            cout << "False length";
-            return false;
-        }
+		if (passwordCheck.length() < 8 || passwordCheck.length() > 64) {
+			cout << "Length of password is invalid" << endl;
+			cout << "False length";
+			return false;
+		}
 
-        for (int i = 0; i < password.length(); i++) {
-            if (isspace(passwordCheck[i])) {
-                cout << "False space check";
-                return false;
-            }
-            if (isupper(passwordCheck[i])) {
-                lettersUpper++;
-            }
-            if (islower(passwordCheck[i])) {
-                lettersLower++;
-            }
-            if (!isalnum(passwordCheck[i]) && !isspace(passwordCheck[i])) {
-                symbols++;
-            }
-            if (isdigit(passwordCheck[i])) {
-                numbers++;
-            }
-        }
-        cout << "Upper Letters: " << lettersUpper << " Lower Letters: " << lettersLower << " Symbols: " << symbols << " Numbers: " << numbers << endl;
+		for (int i = 0; i < password.length(); i++) {
+			if (isspace(passwordCheck[i])) {
+				cout << "False space check";
+				return false;
+			}
+			else if (isupper(passwordCheck[i])) {
+				lettersUpper++;
+			}
+			else if (islower(passwordCheck[i])) {
+				lettersLower++;
+			}
+			else if (!isalnum(passwordCheck[i]) && !isspace(passwordCheck[i])) {
+				symbols++;
+			}
+			else if (isdigit(passwordCheck[i])) {
+				numbers++;
+			}
+		}
+		cout << "Upper Letters: " << lettersUpper << " Lower Letters: " << lettersLower << " Symbols: " << symbols << " Numbers: " << numbers << endl;
 
-        if (lettersUpper >= 2 && lettersLower >= 2 && numbers >= 2 && symbols >= 1) {
-            cout << "True";
-            return true;
-        }
-        else {
-            cout << "False last check";
-            return false;
-        }
-    }
+		if (lettersUpper >= 2 && lettersLower >= 2 && numbers >= 2 && symbols >= 1) {
+			cout << "True";
+			return true;
+		}
+		else {
+			cout << "False last check";
+			return false;
+		}
+	}
 
-    void logIn()
-    {
-        cout << "Lalala";
-    }
+	void logIn()
+	{
+		while (true) {
+			if (!isLogIn) {
+				cout << "Please enter your E-Mail: " << endl;
+				cin >> emailLogIn;
+				if (emailLogIn == email) {
+					cout << "Please enter your password: " << endl;
+					cin >> passwordLogIn;
+					if (passwordLogIn == password) {
+						cout << "Log in succesfull! Welcome " << username << endl;
+						isLogIn = true;
+						break;
+					}
+					else {
+						cout << "Oops, the password you entered is incorrect" << endl;
+					}
+				}
+				else {
+					cout << "Oops, the email you entered is incorrect" << endl;
+				}
+			}
+			else {
+				cout << "Oops, you are already logged in" << endl;
+			}
+		}
+	}
 
-    void buySkyrim()
-    {
-        cout << "Skytom";
-    }
+	void buySkyrim()
+	{
+		cout << "Skytom";
+	}
 };
 
 int main()
 {
 
-    Account user1;
-    user1.signUp();
-    user1.isValidPassword("c3K1u=£1sZ{m");
-    return 0;
+	Account user1;
+	user1.signUp();
+	user1.isValidPassword("c3K1u=£1sZ{m");
+	return 0;
 }
